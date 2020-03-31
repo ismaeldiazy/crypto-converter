867c1f274ea6298d068b256c0135c00fcc10f48f
ismaeldiazy <ismaeldiazy@gmail.com>
2020-03-31 04:06:15 +0200

    The method print result from Interface class displays now the last day variation of the crypto coin and the last update of the data

--- js/ui.js
+++ js/ui.js
@@ -44,15 +44,21 @@ class Interface {
     printResult(response, coin, crypto) {
         
         const coinData = response[crypto][coin];
+        console.log(coinData);
 
         // // Reduce decimal numbers to 2 digits
-        let price = coinData.PRICE.toFixed(2);
+        let price = coinData.PRICE.toFixed(2),
+            percentage = coinData.CHANGEPCTDAY.toFixed(2),
+            // Format UNIX timestamp to date
+            updateDate = new Date(coinData.LASTUPDATE * 1000).toLocaleDateString('es-ES');
         // Create template
         let templateHTML = `
             <div class="card bg-success">
                 <div class="card-body text-light">
                     <h2 class="card-title">Result:</h2>
                     <p>The value of 1 ${coinData.FROMSYMBOL} converted to ${coinData.TOSYMBOL} is ${price} </p>
+                    <p>Last day variation: ${percentage}</p>
+                    <p>Last update: ${updateDate}</p>
                 </div>
             </div>
         `
